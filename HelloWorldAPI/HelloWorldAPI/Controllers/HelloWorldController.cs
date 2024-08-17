bfa88af3910465c993a810e3ab1f26d17f74c05b
Sujith <sujith.kp@hotmail.com>
2024-08-17 22:42:02 +0530

    Initial commit.

---
+++ HelloWorldAPI/HelloWorldAPI/Controllers/HelloWorldController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloWorldAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloWorldController : ControllerBase
+    {
+        public ActionResult Index()
+        {
+            return new JsonResult(new { Message = "Hello Woeld !" });
+        }
+    }
+}
